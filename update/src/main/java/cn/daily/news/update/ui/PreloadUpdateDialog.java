8b39f2aa7aa0fb648a5c0e216319a695b3ff1a23
李欣科 <lixink@8531.cn>
2019-10-29 18:42:15 +0800

    预加载

--- update/src/main/java/cn/daily/news/update/ui/PreloadUpdateDialog.java
+++ update/src/main/java/cn/daily/news/update/ui/PreloadUpdateDialog.java
@@ -1,6 +1,15 @@
 package cn.daily.news.update.ui;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.Gravity;
 import android.view.View;
 
 import cn.daily.news.update.R;
@@ -11,6 +20,13 @@ import cn.daily.news.update.UpdateType;
  */
 
 public class PreloadUpdateDialog extends UpdateDialogFragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTitleView.setGravity(Gravity.LEFT);
+    }
+
     @Override
     protected String getOKText() {
         return getString(R.string.update_ok);
@@ -28,7 +44,13 @@ public class PreloadUpdateDialog extends UpdateDialogFragment {
 
     @Override
     protected SpannableString getTitle() {
-        return new SpannableString(getString(R.string.text_title_preload, mLatestBean.version));
+        String title = getString(R.string.text_title_preload) + "\t";
+        String version = " " + mLatestBean.version + " ";
+        SpannableString spannableString = new SpannableString(title + version);
+        spannableString.setSpan(new AbsoluteSizeSpan(12, true), title.length(), title.length() + version.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#ffffff")), title.length(), title.length() + version.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new BackgroundColorSpan(getResources().getColor(R.color.update_top_title_tip_color)), title.length(), title.length() + version.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannableString;
     }
 
     @Override
